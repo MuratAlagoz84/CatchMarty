f558ca5de031267523f22b64e868eb29d2712a2e
Murat Alagöz <muratalagoz84@gmail.com>
2019-10-24 15:29:40 +0300

    Initial Commit

---
+++ CatchMarty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchMarty
+//
+//  Created by Murat Alagöz on 24.10.2019.
+//  Copyright © 2019 Murat Alagöz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
