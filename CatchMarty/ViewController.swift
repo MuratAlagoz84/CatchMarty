3758c8806376273695662de1462d82334360263e
Murat Alagöz <muratalagoz84@gmail.com>
2019-10-25 15:01:27 +0300

    addet UI links

--- CatchMarty/ViewController.swift
+++ CatchMarty/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet weak var marty1: UIImageView!
+    @IBOutlet weak var marty2: UIImageView!
+    @IBOutlet weak var marty3: UIImageView!
+    @IBOutlet weak var marty4: UIImageView!
+    @IBOutlet weak var marty5: UIImageView!
+    @IBOutlet weak var marty6: UIImageView!
+    @IBOutlet weak var marty7: UIImageView!
+    @IBOutlet weak var marty8: UIImageView!
+    @IBOutlet weak var marty9: UIImageView!
+    @IBOutlet weak var marty10: UIImageView!
+    @IBOutlet weak var marty11: UIImageView!
+    @IBOutlet weak var marty12: UIImageView!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+        
+        
+        
+        
     }
 
 
